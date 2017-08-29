3eec7b0223444fcf46981da13818ce3aea0ef7f5
67891abc <1768300180abc@gmail.com>
2017-08-29 14:24:14 +0800

    hermes: Initial push

---
+++ lineage.mk
@@ -0,0 +1,27 @@
+# Inherit device configuration
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Device display
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hermes
+PRODUCT_NAME := lineage_hermes
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_RELEASE_NAME := hermes
+PRODUCT_RESTRICT_VENDOR_FILES := false
+PRODUCT_MODEL := Redmi Note 2
+
+# SDcard
+PRODUCT_CHARACTERISTICS := nosdcard
+
+
+
+
+
+
